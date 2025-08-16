0e729c43e2b7a57ab621223689a91806c48491f0
Trofymenko Vladyslav <naughtysora@proton.me>
2025-08-16 18:54:58 +0300

    ref counter no tests

--- main.js
+++ main.js
@@ -1,1 +1,58 @@
 'use strict';
+
+class RefCounter {
+  #entity = null;
+  #dispose = null;
+  #references = 0;
+  #dropped = false;
+  #originalDispose = null;
+
+  constructor(entity, onDispose) {
+    const original = this.#originalDispose = entity?.[Symbol.dispose];
+    this.#dispose = original ?? onDispose ?? null;
+    if (this.#dispose === null) {
+      throw new Error("Can't find references' [Symbol.dispose] method");
+    }
+    Object.defineProperty(entity, Symbol.dispose, {
+      value: () => {
+        this.#references--;
+        if (this.#references !== 0) return;
+        this.#dispose.call(this.#entity, this.#entity);
+        this.drop();
+      },
+      enumerable: false,
+      writable: true,
+      configurable: true,
+    });
+    this.#entity = entity;
+  }
+
+  ref() {
+    if (this.#dropped) {
+      throw new Error("Reference was dropped");
+    }
+    this.#references++;
+    return this.#entity;
+  }
+
+  drop() {
+    if (this.#dropped) return;
+    Object.defineProperty(this.#entity, Symbol.dispose, {
+      value: this.#originalDispose,
+      enumerable: false,
+      writable: true,
+      configurable: true,
+    });
+    this.#entity = null;
+    this.#dispose = null;
+    this.#references = 0;
+    this.#dropped = true;
+    this.#originalDispose = null;
+  }
+
+  get dropped() {
+    return this.#dropped;
+  }
+}
+
+module.exports = { RefCounter };
--- test/index.js
+++ test/index.js
@@ -1,2 +1,61 @@
 'use strict';
 
+const assert = require("node:assert");
+const { describe, it } = require("node:test");
+const { RefCounter } = require("../main");
+
+// const getCounter = (counter) => {
+//   using c = counter.ref();
+//   console.log("counter end", c);
+// };
+
+// {
+//   // usage
+//   const free = (a) => void console.log(`dispose ${JSON.stringify(a)}`, a.value = 0);
+//   const smile = console.log.bind(null, "free :3");
+//   const v = { value: 5 }
+//   const counter = new RefCounter(v, free);
+//   {
+//     using a = counter.ref();
+//     console.log("inner end a", a, v);
+//     {
+//       using b = counter.ref();
+//       console.log("inner end b", b, v);
+//     }
+//     // counter.drop();
+//     console.log("check counter ref dropped", counter.dropped);
+//     getCounter(counter);
+//   }
+//   console.log("end", v, "check counter ref dropped", counter.dropped);
+// }
+
+
+// {
+//   // usage
+//   const free = (a) => void console.log(`dispose ${JSON.stringify(a)}`, a.value = 0);
+//   const v = {
+//     value: 5,
+//     [Symbol.dispose](self) {
+//       console.log("BOOM!");
+//       this.value = Math.random();
+//     }
+//   };
+//   const counter = new RefCounter(v, free);
+//   {
+//     using a = counter.ref();
+//     console.log("inner end a", a, v);
+//     {
+//       using b = counter.ref();
+//       console.log("inner end b", b, v);
+//     }
+//     // counter.drop();
+//     console.log("check counter ref dropped", counter.dropped);
+//     getCounter(counter);
+//   }
+//   console.log("end", v, "check counter ref dropped", counter.dropped, counter);
+
+//   {
+//     using test = v;
+//   }
+//   console.log(v)
+// }
